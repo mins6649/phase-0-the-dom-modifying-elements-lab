ab34850d5542dd02dcac37f5ed95974de54b4341
minyoungshin <mins6649@gmail.com>
2023-02-07 12:52:56 -0500

    dom modify

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const body = document.querySelector("body")
+//you dont need to define body to use LINE 14
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+function champion(name){
+    newHeader.textContent = `${name} is the champion`;
+}
+champion("Min");
+
+document.body.append(newHeader);
